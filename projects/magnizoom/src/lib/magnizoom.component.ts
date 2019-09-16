9de334e028654a5af55411de4e0d844bc821f06f
Henczidai Bálint <henczidaibalint@gmail.com>
2019-09-16 14:14:20 +0200

    cover mode & custom style, class

--- projects/magnizoom/src/lib/magnizoom.component.ts
+++ projects/magnizoom/src/lib/magnizoom.component.ts
@@ -11,8 +11,12 @@ type Point2D = { x: number, y: number } | null;
 export class MagnizoomComponent implements OnInit {
 
   @Input() imageSrc: string;
+  @Input() zoomMode: 'LENS' | 'COVER' = 'COVER';
   @Input() minZoomFactor = 1.2;
   @Input() maxZoomFactor = 3;
+
+  @Input() imageStyle: { [x: string]: any; };
+  @Input() imageClass: any;
 
   @ViewChild('mainCanvas', { static: true }) mainCanvasRef: ElementRef;
 
@@ -21,7 +25,7 @@ export class MagnizoomComponent implements OnInit {
   image: HTMLImageElement;
 
   mousePosition: Point2D = null;
-  zoomSize: Size2D = { width: 400, height: 300 };
+  lensSize: Size2D = { width: 400, height: 300 };
   zoomFactor = 2;
 
   get canvasWidth() { return this.image && this.image.width || 800; }
@@ -42,7 +46,7 @@ export class MagnizoomComponent implements OnInit {
   loadImage(src: string) {
     this.image = new Image();
     this.image.onload = () => {
-      this.zoomSize = { width: this.image.width / 2, height: this.image.height / 2 };
+      this.lensSize = { width: this.image.width / 2, height: this.image.height / 2 };
       this.render();
     };
     this.image.src = src;
@@ -50,39 +54,67 @@ export class MagnizoomComponent implements OnInit {
 
   render() {
     this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight); // clear canvas
-    this.context.lineWidth = 5; // border width
+    this.context.lineWidth = 1; // border width
     this.context.drawImage(this.image, 0, 0); // bg image
+
     if (this.mousePosition) {
-      const zoomRect = this.getZoomRect();
-      this.context.fillRect(zoomRect.x, zoomRect.y, zoomRect.w, zoomRect.h); // bg (clear)
-      const clippingRect = this.getClippingRect();
-      // zoom image
-      this.context.drawImage(
-        this.image,
-        clippingRect.x, clippingRect.y, clippingRect.w, clippingRect.h,
-        zoomRect.x, zoomRect.y, zoomRect.w, zoomRect.h
-      );
-      this.context.strokeRect(zoomRect.x, zoomRect.y, zoomRect.w, zoomRect.h); // border
+      switch (this.zoomMode) {
+        case 'LENS': this.renderLensMode(); break;
+        case 'COVER': this.renderCoverMode(); break;
+      }
     }
   }
 
+  renderLensMode() {
+    this.context.lineWidth = 5; // border width
+    const zoomRect = this.getZoomRect();
+    this.context.fillRect(zoomRect.x, zoomRect.y, zoomRect.w, zoomRect.h); // bg (clear)
+    const clippingRect = this.getClippingRect();
+    // zoom image
+    this.context.drawImage(
+      this.image,
+      clippingRect.x, clippingRect.y, clippingRect.w, clippingRect.h,
+      zoomRect.x, zoomRect.y, zoomRect.w, zoomRect.h
+    );
+    this.context.strokeRect(zoomRect.x, zoomRect.y, zoomRect.w, zoomRect.h); // border
+  }
+
+  renderCoverMode() {
+    const covertRect = this.getCoverRect();
+    this.context.drawImage(
+      this.image,
+      covertRect.x, covertRect.y, covertRect.w, covertRect.h,
+      0, 0, this.canvasWidth, this.canvasHeight
+    ); // cover image
+  }
+
   getZoomRect() {
-    const w = this.zoomSize.width;
-    const h = this.zoomSize.height;
-    let x = this.mousePosition.x - (w / 2);
-    let y = this.mousePosition.y - (h / 2);
-    if (x <= 0) { x = 0; }
-    if (x + w >= this.canvasWidth) { x = this.canvasWidth - w; }
-    if (y < 0) { y = 0; }
-    if (y + h >= this.canvasHeight) { y = this.canvasHeight - h; }
-    return { x, y, w, h };
+    const w = this.lensSize.width;
+    const h = this.lensSize.height;
+    const x = this.mousePosition.x - (w / 2);
+    const y = this.mousePosition.y - (h / 2);
+    return this.clampRect(x, y, w, h);
   }
 
   getClippingRect() {
-    const w = this.zoomSize.width / this.zoomFactor;
-    const h = this.zoomSize.height / this.zoomFactor;
-    let x = this.mousePosition.x - (w / 2);
-    let y = this.mousePosition.y - (h / 2);
+    const w = this.lensSize.width / this.zoomFactor;
+    const h = this.lensSize.height / this.zoomFactor;
+    const x = this.mousePosition.x - (w / 2);
+    const y = this.mousePosition.y - (h / 2);
+    return this.clampRect(x, y, w, h);
+  }
+
+  getCoverRect() {
+    const w = this.canvasWidth / this.zoomFactor;
+    const h = this.canvasHeight / this.zoomFactor;
+    // const x = this.mousePosition.x - (w / 2);
+    // const y = this.mousePosition.y - (h / 2);
+    const x = this.mousePosition.x - this.mousePosition.x / this.zoomFactor;
+    const y = this.mousePosition.y - this.mousePosition.y / this.zoomFactor;
+    return this.clampRect(x, y, w, h);
+  }
+
+  clampRect(x: number, y: number, w: number, h: number) {
     if (x <= 0) { x = 0; }
     if (x + w >= this.canvasWidth) { x = this.canvasWidth - w; }
     if (y < 0) { y = 0; }
--- projects/magnizoom/src/lib/magnizoom.module.ts
+++ projects/magnizoom/src/lib/magnizoom.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MagnizoomComponent } from './magnizoom.component';
+import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
   declarations: [MagnizoomComponent],
   imports: [
+    CommonModule
   ],
   exports: [MagnizoomComponent]
 })
